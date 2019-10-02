134a81c91ffba88c5d43560c4599730cc6a3f879
zFitness <zhengmiaode@hotmail.com>
2019-10-02 22:19:41 +0800

    init repo

---
+++ src/main/java/cn/zm/community/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package cn.zm.community.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zfitness
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
