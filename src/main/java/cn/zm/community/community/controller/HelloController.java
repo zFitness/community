d6d956dda894f7f8698c4cd6bc6836d74b945568
zFitness <zhengmiaode@hotmail.com>
2019-10-03 09:07:02 +0800

    add static

--- src/main/java/cn/zm/community/community/controller/HelloController.java
+++ src/main/java/cn/zm/community/community/controller/HelloController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/index")
     public String hello(@RequestParam(name = "name", required = false, defaultValue = "world") String name, Model model) {
         model.addAttribute("name", name);
-        return "hello";
+        return "index";
     }
 }
