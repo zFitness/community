2c7bdcd033cabd5bea6502401b06dc15ed3000e3
zFitness <zhengmiaode@hotmail.com>
2019-10-04 15:22:15 +0800

    add:publish

--- src/main/java/cn/zm/community/community/controller/PublishController.java
+++ src/main/java/cn/zm/community/community/controller/PublishController.java
@@ -1,15 +1,68 @@
 package cn.zm.community.community.controller;
 
+import cn.zm.community.community.mapper.QuestionMapper;
+import cn.zm.community.community.mapper.UserMapper;
+import cn.zm.community.community.model.Question;
+import cn.zm.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author zfitness
  */
 @Controller
 public class PublishController {
+    @Autowired
+    private QuestionMapper questionMapper;
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish() {
+        return "publish";
+    }
+
+    @PostMapping("/publish")
+    private String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model
+    ) {
+        Cookie[] cookies = request.getCookies();
+
+
+        String token = null;
+        User user = null;
+        for (Cookie cookie : cookies) {
+            if ("token".equals(cookie.getName())) {
+                token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+        }
+        Question question = new Question();
+        question.setTag(tag);
+        question.setTitle(title);
+        question.setDescription(description);
+
+        question.setCreator("");
+        questionMapper.create(question);
+
         return "publish";
     }
 }
---
+++ src/main/java/cn/zm/community/community/mapper/QuestionMapper.java
@@ -0,0 +1,15 @@
+package cn.zm.community.community.mapper;
+
+import cn.zm.community.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author zfitness
+ */
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question(title, description, gmt_modified,gmt_create,creator,tag) " +
+            "values(#{title}, #{description}, #{gmtModified}, #{gmtCreate},#{creator}, #{tag}")
+    public void create(Question question);
+}
